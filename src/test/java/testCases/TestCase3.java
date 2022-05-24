073f935126f415f5019bc9c37614601c49d88795
Ramazan Yasar <ramazanyas@gmail.com>
2022-05-24 23:43:33 +0200

    24/05/2022

--- src/test/java/testCases/TestCase3.java
+++ src/test/java/testCases/TestCase3.java
@@ -1,13 +1,39 @@
 package testCases;
 
-public class TestCase3 {
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
 
-    //1. Launch browser
-    //2. Navigate to url 'http://automationexercise.com'
-    //3. Verify that home page is visible successfully
-    //4. Click on 'Signup / Login' button
-    //5. Verify 'Login to your account' is visible
-    //6. Enter incorrect email address and password
-    //7. Click 'login' button
-    //8. Verify error 'Your email or password is incorrect!' is visible
+public class TestCase3 extends TestBase {
+
+
+
+
+    @Test
+    public void test1() {
+        //1. Launch browser
+        //2. Navigate to url 'http://automationexercise.com'
+        driver.get("http://automationexercise.com");
+        //3. Verify that home page is visible successfully
+        WebElement homePage = driver.findElement(By.xpath("//i[@class='fa fa-home']"));
+        Assert.assertTrue(homePage.isDisplayed());
+        //4. Click on 'Signup / Login' button
+        driver.findElement(By.xpath("//i[@class='fa fa-lock']")).click();
+        //5. Verify 'Login to your account' is visible
+        WebElement loginText = driver.findElement(By.xpath("//*[text()='Login to your account']"));
+        Assert.assertTrue(loginText.isDisplayed());
+        //6. Enter incorrect email address and password
+        driver.findElement(By.xpath("(//input[@name='email'])[1]")).sendKeys("veli@can.com");
+        driver.findElement(By.xpath("(//input[@name='password'])[1]")).sendKeys("123456fr");
+        //7. Click 'login' button
+        driver.findElement(By.xpath("(//button[@class='btn btn-default'])[1]")).click();
+        //8. Verify error 'Your email or password is incorrect!' is visible
+        WebElement incorrectText = driver.findElement(By.xpath("//*[text()='Your email or password is incorrect!']"));
+        Assert.assertTrue(incorrectText.isDisplayed());
+
+
+    }
 }
